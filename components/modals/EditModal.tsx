e116294d4452a35adff57b06a5303c2173107ec6
vishal-codes <itsvishal2417@gmail.com>
2023-04-01 16:19:17 +0530

    feat: add functionality to edit profile
    Signed-off-by: vishal-codes <itsvishal2417@gmail.com>

---
+++ components/modals/EditModal.tsx
@@ -0,0 +1,110 @@
+import { useCallback, useEffect, useState } from 'react';
+import { toast } from 'react-hot-toast';
+import axios from 'axios';
+
+import useEditModal from '@/hooks/useEditModal';
+import useCurrentUser from '@/hooks/useCurrentUser';
+import useUser from '@/hooks/useUser';
+import ImageUpload from '../ImageUpload';
+import Modal from '../Modal';
+import Input from '../Input';
+
+const EditModal = () => {
+    const { data: currentUser } = useCurrentUser();
+    const { mutate: mutateFetchedUser } = useUser(currentUser?.id);
+    const editModal = useEditModal();
+
+    const [profileImage, setProfileImage] = useState<string>('');
+    const [coverImage, setCoverImage] = useState<string>('');
+    const [name, setName] = useState<string>('');
+    const [username, setUsername] = useState<string>('');
+    const [bio, setBio] = useState<string>('');
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    useEffect(() => {
+        setProfileImage(currentUser?.profileImage);
+        setCoverImage(currentUser?.coverImage);
+        setName(currentUser?.name);
+        setUsername(currentUser?.username);
+        setBio(currentUser?.bio);
+    }, [currentUser]);
+
+    const onSubmit = useCallback(async () => {
+        console.log(bio);
+        try {
+            setIsLoading(true);
+            await axios.patch('/api/edit', {
+                name,
+                username,
+                bio,
+                profileImage,
+                coverImage,
+            });
+            mutateFetchedUser();
+            toast.success('Profile updated successfully!');
+            editModal.onClose();
+        } catch (error) {
+            toast.error('Something went wrong!');
+            console.log(error);
+        } finally {
+            setIsLoading(false);
+        }
+    }, [
+        name,
+        username,
+        bio,
+        profileImage,
+        coverImage,
+        editModal,
+        mutateFetchedUser,
+    ]);
+
+    const bodyContent = (
+        <div className='flex flex-col gap-4'>
+            <ImageUpload
+                value={profileImage}
+                disabled={isLoading}
+                onChange={(image) => setProfileImage(image)}
+                label='Upload profile image'
+            />
+            <ImageUpload
+                value={coverImage}
+                disabled={isLoading}
+                onChange={(image) => setCoverImage(image)}
+                label='Upload cover image'
+            />
+            <Input
+                placeholder='Name'
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+                disabled={isLoading}
+            />
+            <Input
+                placeholder='Username'
+                onChange={(e) => setUsername(e.target.value)}
+                value={username}
+                disabled={isLoading}
+            />
+            <Input
+                placeholder='Bio'
+                onChange={(e) => setBio(e.target.value)}
+                value={bio}
+                disabled={isLoading}
+            />
+        </div>
+    );
+
+    return (
+        <Modal
+            disabled={isLoading}
+            isOpen={editModal.isOpen}
+            title='Edit your profile'
+            actionLabel='Save'
+            onClose={editModal.onClose}
+            onSubmit={onSubmit}
+            body={bodyContent}
+        />
+    );
+};
+
+export default EditModal;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,11 +6,13 @@ import { SessionProvider } from 'next-auth/react';
 import Layout from '@/components/Layout';
 import LoginModal from '@/components/modals/LoginModal';
 import RegisterModal from '@/components/modals/RegisterModal';
+import EditModal from '@/components/modals/EditModal';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <SessionProvider session={pageProps.session}>
             <Toaster />
+            <EditModal />
             <LoginModal />
             <RegisterModal />
             <Layout>
